e2bfc202aa8a64862a410d249219a55b227fb4c1
Glitch (hirsizyunusun) <none>
2020-05-27 10:37:02 +0000

    🍏🙍 Checkpoint ./config.js:10873923/59

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const config = {
-    "ownerID": "282453998529806338", //kendi IDınızı yazınız
-    "admins": [""],
-    "support": [""],
-    "token": "", //botunuzun tokenini yazınız
+    "ownerID": "236173144300191754", //kendi IDınızı yazınız
+    "admins": ["236173144300191754236173144300191754"],
+    "support": ["236173144300191754"],
+    "token": "NzEwOTI2NTg1ODA0MzU3Njky.Xs5A4g.WQTJXb0Qx94L_l1GsxD3DLbPKGg", //botunuzun tokenini yazınız
     "dashboard" : {
-      "oauthSecret": "", //botunuzun secretini yazınız
-      "callbackURL": `https://dash.vortexbot.org/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
+      "oauthSecret": "JmZhVq42pBQ_zIwru6Mh02jcJ_BrTrta", //botunuzun secretini yazınız
+      "callbackURL": `https://vortex-altyaps.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
       "sessionSecret": "", //kalsın
-      "domain": "https://dash.vortexbot.org/", //site URLnizi yazınız!
+      "domain": "https://vortex-altyaps.glitch.me", //site URLnizi yazınız!
           "port": process.env.PORT
     }
   };
