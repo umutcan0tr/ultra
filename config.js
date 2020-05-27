f303a3ab15d88cd924bd38cfab04ca3451dcdeea
Glitch (vortex-altyaps) <none>
2020-05-27 20:38:38 +0000

    🦀🎏 Checkpoint ./ayarlar.json:10873923/36 ./config.js:10873923/346 ./index.js:10873923/118 ./modüller/panel.js:10873923/128 ./config.json:10873923/118

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = {
     "ownerID": "236173144300191754", //kendi IDınızı yazınız
     "admins": ["236173144300191754"],
     "support": ["236173144300191754"],
-    "token": "NzEwOTI2NTg1ODA0MzU3Njky.Xs5y1w.xVHrn2OgJ0DCJMXZ_poA-nskIAU", //botunuzun tokenini yazınız
+    "token": "NzEwOTI2NTg1ODA0MzU3Njky.Xs7OYA.hFNt1MWxWSo5UtUSjpkiwtsSH-c", //botunuzun tokenini yazınız
     "dashboard" : {
       "oauthSecret": "Ku430cPleZhNeJ_Zi6jSjPUo4YhIrumh", //botunuzun secretini yazınız
       "callbackURL": `https://vortex-altyaps.glitch.me/callback`, //site URLnizi yazınız /callback kısmını silmeyiniz!
